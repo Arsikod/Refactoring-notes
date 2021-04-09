e10751cda0899b3a493ff393785df58317141550
Nurbol <nurbol7@gmail.com>
2021-04-09 18:57:30 +0300

    ch 1

---
+++ Phrases.md
@@ -0,0 +1,15 @@
+**_Motivation_**
+
+-The code slowly sinks from engineering to hacking.
+
+-When you have to add a feature to a program but the code is not structured in a convenient way, first refactor the program to make it easy to add the feature, then add the feature.
+
+-As an experienced developer I can be sure that whatever scheme they come up with, they will change it again within six months. After all, when feature requests come, they come not as single spies but in battalions.
+
+-Essence of the refactoring process: small changes and testing after each change.
+
+-It's my coding standard to always call the return value from a function "result". That way I always know its role.
+
+-With good names, I don't have to read the body of the function to see what it does.
+
+-My overall advice on performance with refactoring is: Most of the time you should ignore it. If your refactoring introduces performance slow-downs, finish refactoring first and do performance tuning afterwards.
---
+++ chapter 1 demo/app.js
@@ -0,0 +1,129 @@
+const invoices = require("./invoices").invoices;
+const plays = require("./plays").plays;
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error("subclass responsibility");
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40_000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30_000;
+    if (this.performance.audience > 20) {
+      result += 10_000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
+function enrichPerformance(aPerformance) {
+  const calculator = createPerformanceCalculator(
+    aPerformance,
+    playFor(aPerformance)
+  );
+  const result = Object.assign({}, aPerformance);
+  result.play = calculator.play;
+  result.amount = calculator.amount;
+  result.volumeCredits = calculator.volumeCredits;
+  return result;
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+
+    default:
+      throw new Error(`unknow type: ${aPlay.type}`);
+  }
+}
+
+function statement(invoice) {
+  return renderPlainText(createStatementData(invoice));
+}
+
+function createStatementData(invoice) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+
+  return result;
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats) \n`;
+  }
+
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
+}
+
+function totalAmount(data) {
+  return data.performances.reduce((total, p) => total + p.amount, 0);
+}
+
+function totalVolumeCredits(data) {
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+
+  //add extra credit for every ten comedy attendees
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+
+  return result;
+}
+
+console.log(statement(invoices));
